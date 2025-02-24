ab7f005780c6a5c0c1720f9ec631f17506f72f45
PRATIK DIPAK PATIL <coolpratik9575@gmail.com>
2025-02-24 12:01:58 +0530

    managed old missing code with fix to the some files

--- lib/features/business_directory/presentation/business_add_screen.dart
+++ lib/features/business_directory/presentation/business_add_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:ns_community_support_hub/core/app_routes/route_names.dart';
 import 'package:ns_community_support_hub/core/common_widgets/custom_app_bar.dart';
 import 'package:ns_community_support_hub/core/common_widgets/footer_bar.dart';
@@ -8,7 +7,7 @@ import 'package:ns_community_support_hub/core/common_widgets/hero_section_with_p
 import 'package:ns_community_support_hub/core/local/local_strings.dart';
 import 'package:ns_community_support_hub/features/business_directory/business_directory_provider.dart';
 import 'package:ns_community_support_hub/features/business_directory/presentation/widgets/add_business_form.dart';
-import 'package:ns_community_support_hub/features/events/presentation/widgets/add_event_form.dart';
+
 import 'package:provider/provider.dart';
 
 class AddBusinessScreen extends StatefulWidget {
